eab931f51f8ba831ef08503fb0ff4d98056324d5
Sbklopp <sbklopp11@gmail.com>
2022-11-18 10:29:09 -0500

    finished method

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # endpoint_demo
 
 https://www.twilio.com/blog/2015/10/4-ways-to-parse-a-json-api-with-ruby.html
+
+https://localhost:7139/api/employee
--- rest.rb
+++ rest.rb
@@ -1,3 +1,42 @@
 require 'openssl'
    OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+require 'net/http'
+require 'json'
 
+
+# https://localhost:7139/api/employee
+
+
+def title_select(email_str)
+
+    #this parses our endpoint and returns JSON
+    url = 'https://localhost:7139/api/employee'
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    employees = JSON.parse(response)
+
+
+    #empty list for correct matches
+    matches = []
+
+    #loop through each row of employees 
+    employees.each do |row|
+        # if employee email matches the string provided 
+        #we add the employee title to our matches list
+        if row['email'] == email_str
+            matches << row['title']
+        end
+
+    end
+    
+    
+    if matches.count > 0
+        return matches[0]
+    else 
+        return "email not found"
+    end 
+
+end
+
+
+puts title_select("add email here to test")
